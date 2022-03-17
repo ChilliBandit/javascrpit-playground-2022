7a33c25d45f0e22edd2f35d1b451c34d3ec62e5e
hh hh <phoenix-binniegenet@live.nmit.ac.nz>
2022-03-17 18:56:30 +1300

    js in class examples

--- src/week-4.js
+++ src/week-4.js
@@ -2,45 +2,139 @@
 /*
 JavaScript Variables
 */
-var fullName = "Phoenix Binnie-Genet"
-var classCode = 503
+// var fullName = "Phoenix Binnie-Genet"
+// var classCode = 503
 
-console.log(fullName)
-console.log(classCode)
+// console.log(fullName)
+// console.log(classCode)
 
-// JavaScript Objects 
+// // JavaScript Objects 
 
-var full_name = 
-{
-first_name: "Phoenix",
-last_name: "Binnie-Genet",
-course_number: 503
-}
+// var full_name = 
+// {
+// first_name: "Phoenix",
+// last_name: "Binnie-Genet",
+// course_number: 503
+// }
 
-console.log(full_name)
-console.log(full_name.first_name)
-console.log(full_name["last_name"])
+// console.log(full_name)
+// console.log(full_name.first_name)
+// console.log(full_name["last_name"])
 
-// Javascript Functions
-function addTwoNumbers(num1, num2) {
-    return num1+num2
-}
-console.log(addTwoNumbers(12,3))
+// // Javascript Functions
+// function addTwoNumbers(num1, num2) {
+//     return num1+num2
+// }
+// console.log(addTwoNumbers(12,3))
 
-function addThreeNumbers(num1, num2, num3) {
-    return num1+num2+num3
-}
-console.log(addThreeNumbers(69,420,1337))
+// function addThreeNumbers(num1, num2, num3) {
+//     return num1+num2+num3
+// }
+// console.log(addThreeNumbers(69,420,1337))
 
-// JavaScrpit Loops 
+// // JavaScrpit Loops 
 
-for(var i=0; i< 10; i++){
-    console.log(i)
-}
-//JavaScript if statements
-var number = 9
-if (number === 10) {
-    console.log("answer = " + number)
-} else {
-    console.log("error")
-}
+// for(var i=0; i< 10; i++){
+//     console.log(i)
+// }
+// //JavaScript if statements
+// var number = 9
+// if (number === 10) {
+//     console.log("answer = " + number)
+// } else {
+//     console.log("error")
+// }
+
+// // const and let
+// let num = 3
+// console.log(num)
+
+// num = 5
+// console.log(num)
+
+// // JavaScript comments
+
+// /*
+// Hello world 
+// class SDV503
+// */
+// var sdv503 = "javascript"
+
+// var x = 3
+
+// var Full_Name = "text"
+
+
+// function foo(p1, p2) {
+//          return p1 / p2
+// }
+
+// foo(5, 10)
+
+// // var new_value = function sum(x1, x2) {
+// //     return x1 + x2
+// // }
+// // sum(10, 3)
+
+// // (=) vs (===)
+
+// // statements vs expresstions 
+// var numbers 
+
+
+// var x 
+
+// var word = "suighdeishdfgagagagaggai"
+// console.log(word.length)
+
+// var obj = {}
+// //console.log(obj)
+
+// obj.foo = 123
+// console.log(obj)
+// console.log(word.toUpperCase())
+
+// JavaScript values (primitives vs objects)
+
+// var obj1 = {}
+// var obj2 = {}
+
+// console.log(obj1 === obj1)
+
+// var number1 = 123
+// var number2 = 123
+
+// console.log(number1 === number2)
+
+//Boolean: True and false
+//Numbers: 1234, 24323.3
+//Strings 'ajhsjdh', "hiasshsdj"
+//non-values: null and undefinded 
+
+// var str = "Binnie-Genet"
+// console.log(str.length = 2)
+// console.log(str.length)
+
+// var numbers = [1, 2, 3, 4, 5]
+// console.log(numbers)
+// console.log(numbers[3] = 10)
+// console.log(numbers)
+
+//({} === {})
+
+//non values 
+// var nonvalue
+// console.log(nonvalue)
+
+// function nonvalue(x) {
+//     return x
+// }
+
+// console.log(nonvalue())
+
+ var value = {}
+ console.log(typeof value)
+// console.log(nonvalue.first_name)
+
+//if (value === undefined || value === null)
+//false, 0, '', NaN, false, [], {}
