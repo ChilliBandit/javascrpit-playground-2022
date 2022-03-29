cca14f41ce0bc06fbc9e01a8ed1865df51b488c8
hh hh <phoenix-binniegenet@live.nmit.ac.nz>
2022-03-30 10:49:19 +1300

    e

--- src/app.js
+++ src/app.js
@@ -1,4 +1,15 @@
-console.log("Hello World")
-console.dir(document)
-console.table({first:"test",val:9});
-console.error("Hello World")
+// console.log("Hello World")
+// console.dir(document)
+// console.table({first:"test",val:9});
+// console.error("Hello World")
+
+// console.log("hello")
+// var a = "hello"
+// a
+// console.log(a);
+
+var myName = "Laurence";
+var myCourse = "JavaScript";
+console.log(myName);
+var myAge = 40;
+console.log(myAge);
